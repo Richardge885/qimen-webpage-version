016672a14c8e3b785800a30fbdde115254f313e7
Richard Ge <richardge1998@hotmail.com>
2024-10-27 15:16:06 +0000

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+// import { paipan } from 'qimen-mingfa';
+import image from './assets/image.jpg';
+
 function App() {
-  return <div>This Should be the only tag in the page</div>;
+    // const result = paipan({
+    //     time: {
+    //         year: 2024,
+    //         month: 10,
+    //         day: 18,
+    //         hour: 19,
+    //         minute: 47,
+    //     },
+    //     ziXuanJu: "",
+    //     additionalSettings: {
+    //         traditionalCharacters: false,
+    //         singleCharacter: false,
+    //     },
+    // });
+    // console.log(result);
+    return <div className="text-4xl text-blue-500">Hello world
+
+        <img src={image} alt="" />
+    </div>;
 }
 
 export default App;
