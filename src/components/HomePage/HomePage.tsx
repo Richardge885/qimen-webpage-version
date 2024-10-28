13ae49c42fde572bbb8266615d85e0b77d3f9d06
Richard Ge <richardge1998@hotmail.com>
2024-10-28 11:19:13 +0000

    Input Current Time to all input tags

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,38 +1,51 @@
 import HomePageChooseMethodButton from "./HomePageChooseMethodButton";
 import HomePageCurrentTimeButton from "./HomePageCurrentTimeButton";
 
+function getCurrentTimeInNumbers(): { year: number; month: number; day: number; hour: number; minutes: number; } {
+    const now = new Date();
+    return {
+        year: now.getFullYear(),
+        month: now.getMonth() + 1, // Months are zero-based, so we add 1
+        day: now.getDate(),
+        hour: now.getHours(),
+        minutes: now.getMinutes(),
+    };
+}
+
 const HomePage = () => {
     const buttonStyle = "w-[60vw] h-[10vh] bg-red text-bglight text-5xl hover:bg-bgdark hover:text-red rounded-[20px] duration-200 py-10 flex flex-col justify-center items-center";
     const inputLabel = "text-[2rem] text-text font-bold px-[1rem]";
-    const numberInputStyle = "w-[10vw] h-[4vh] text-[3rem] bg-bgdark rounded-[0.48rem]";
+    const numberInputStyle = "w-[17.5vw] h-[4vh] text-[1.5rem] bg-bgdark rounded-[0.48rem] text-center";
+
+    const currentTimeValuesInNumbers = getCurrentTimeInNumbers();
 
     return (
         <div className="flex flex-col bg-bglight h-screen justify-center items-center">
-            <div className="flex flex-col justify-center items-center h-[60vh] gap-[18vh]">
+            <div className="flex flex-col justify-center items-center h-[60vh] gap-[15vh]">
                 <h1 className="font-lishu text-red text-[10rem]">鸣法</h1>
-                <div className="flex flex-col justify-center items-center gap-10">
+                <div className="flex flex-col flex-nowrap justify-center items-center">
                     <div>
                         <label className={inputLabel} htmlFor="year">
                             年
-                            <input type="number" name="year" id="" className={numberInputStyle} min={1} max={9999} />
+                            <input type="number" name="year" id="" className={numberInputStyle} min={1} max={9999} value={currentTimeValuesInNumbers.year} />
                         </label>
                         <label className={inputLabel} htmlFor="month">
                             月
-                            <input type="number" name="month" id="" className={numberInputStyle} min={1} max={12} />
+                            <input type="number" name="month" id="" className={numberInputStyle} min={1} max={12} value={currentTimeValuesInNumbers.month} />
                         </label>
                         <label className={inputLabel}>
                             日
-                            <input type="number" name="day" id="" className={numberInputStyle} min={1} max={31} />
+                            <input type="number" name="day" id="" className={numberInputStyle} min={1} max={31} value={currentTimeValuesInNumbers.day} />
                         </label>
                     </div>
                     <div>
                         <label className={inputLabel}>
                             时
-                            <input type="number" name="hour" id="" className={numberInputStyle} min={0} max={24} />
+                            <input type="number" name="hour" id="" className={numberInputStyle} min={0} max={24} value={currentTimeValuesInNumbers.hour} />
                         </label>
                         <label className={inputLabel}>
                             分
-                            <input type="number" name="minute" id="" className={numberInputStyle} min={0} max={60} />
+                            <input type="number" name="minute" id="" className={numberInputStyle} min={0} max={60} value={currentTimeValuesInNumbers.minutes} />
                         </label>
                     </div>
                 </div>
