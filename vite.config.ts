3dd25c1dce44a36fe1e39207b86fea5cfc75de0e
Richard Ge <richardge1998@hotmail.com>
2024-12-02 20:08:10 +0000

    修复了甲子寻宫空错误

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,5 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
     // plugins: [react(), svgr(), viteSingleFile()],
     plugins: [react(), svgr()],
-    // base: '/qimen-webpage-version/',
-    base: '/',
+    base: '/qimen-webpage-version/',
 });
